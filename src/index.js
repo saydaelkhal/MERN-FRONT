31f650f3c82e41506767e95b4b07d9869e599e9b
saydaelkhal <elkhaleltaiefsaida@gmail.com>
2024-02-23 00:55:19 +0100

    deploy

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import './index.css';
 import App from './App';
 import store from "./redux/store";
 import { Provider } from "react-redux";
+import axios from "axios";
+axios.defaults.baseURL="https://tod-api-j81j.onrender.com";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
